2e07dcd0650e4a38ea7cf1fb1185575b33bb62a8
Havidan <Havidan@g.jct.ac.il>
2022-10-30 12:11:46 +0200

    New print

---
+++ stage0/Program1505.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class program
+    {
+        static void Main(string[]args)
+        {
+            welcome1505();
+            welcome0734();
+            Console.ReadKey();
+
+        }
+        static partial void welcome0734();
+        private static void welcome1505()
+        {
+            Console.WriteLine("rgfds");
+            Console.Write("enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first cosole application", name);
+        }
+    }
+}
