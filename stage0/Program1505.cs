5d62e47599e160e7cace636bc3bf460f2307b776
Havidan <Havidan@g.jct.ac.il>
2022-11-02 21:32:16 +0200

    irhefvdsa

--- stage0/Program1505.cs
+++ stage0/Program1505.cs
@@ -2,20 +2,19 @@
 
 namespace Targil0
 {
-    partial class program
+    partial class Program
     {
         static void Main(string[]args)
         {
             welcome1505();
             welcome0734();
             Console.ReadKey();
+            Console.WriteLine("hgfds");
 
         }
         static partial void welcome0734();
         private static void welcome1505()
         {
-            Console.WriteLine("rgfds");
-            Console.WriteLine("hgfsda" );
             Console.Write("enter your name: ");
             string name = Console.ReadLine();
             Console.WriteLine("{0}, welcome to my first cosole application", name);
