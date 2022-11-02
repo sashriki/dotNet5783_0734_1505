3bfcd2e7359bfcb18711d2745c7a56b0edfe5353
sara shriki <sarunet0401@gmail.com>
2022-11-02 21:16:59 +0200

    uuuuu

--- stage0/Program1505.cs
+++ stage0/Program1505.cs
@@ -14,8 +14,6 @@ namespace Targil0
         static partial void welcome0734();
         private static void welcome1505()
         {
-            Console.WriteLine("rgfds");
-            Console.WriteLine("hgfsda" );
             Console.Write("enter your name: ");
             string name = Console.ReadLine();
             Console.WriteLine("{0}, welcome to my first cosole application", name);
