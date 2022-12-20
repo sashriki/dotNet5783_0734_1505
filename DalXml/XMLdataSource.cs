9a92830d2346eb9dd14c3d9069a902b9ed8d58d8
sara shriki <sarunet0401@gmail.com>
2022-12-20 17:13:26 +0200

    dalOrder and dalOrderItem

--- DalXml/XMLdataSource.cs
+++ DalXml/XMLdataSource.cs
@@ -10,88 +10,197 @@ using DalApi;
 using System.Xml.Linq;
 using System.IO;
 
+using DO;
 namespace Dal;
 
-internal static class XMLdataSource
+internal static class DataSource
 {
-    static string ProductPath = @"xmlProduct.xml";
-    static string OrderPath = @"xmlOrder.xml";
-    static string OrderItemPath = @"xmlOrderItem.xml";
-    static string ConfigPath = @"config.xml";
+    //lists
+    static public List<Product?> products = new List<Product?>();
+    static public List<Order?> orders = new List<Order?>();
+    static public List<OrderItem?> orderItems = new List<OrderItem?>();
 
-    static IDal dal = Factory.Get();
-    static public List<Product?> products = dal.Product.GetAll().ToList();
-    static public List<Order?> orders = dal.Order.GetAll().ToList();
-    static public List<OrderItem?> orderItems = dal.OrderItem.GetAll().ToList();
-    
-    static XMLdataSource()
+    //continuous numbers
+    internal static class Config
     {
-        if(!Directory.Exists(ProductPath))
-            Directory.CreateDirectory(ProductPath);
-        if (!Directory.Exists(OrderPath))
-            Directory.CreateDirectory(OrderPath);
-        if (!Directory.Exists(OrderItemPath))
-            Directory.CreateDirectory(OrderItemPath);
-        if(!Directory.Exists(ConfigPath))
-            Directory.CreateDirectory(ConfigPath);
+        internal static int productIndex = 124568;
+        internal static int orderItemIndex = 200;
+        internal static int orderIndex = 1;
+
     }
-    public static void initialization()
+
+    //randoms
+    static readonly Random random = new Random(); // for the random numbers
+
+    //consts
+    const int InitialNumOfOrders = 100;
+    const int InitialNumOfOrderItems = 20;
+    const int InitialNumOfproducts = 200;
+
+    //initialize
+    static DataSource()
     {
-        Config();
-        InitializationOfProducts();
-        InitializationOfOrder();
-        InitializationOfOrderItems();
+        Init();
     }
-    public static void Config()
+    private static void Init()
     {
-        FileStream fileConfig = new FileStream(ConfigPath, FileMode.Create);
-        XElement ConfigRoot = new XElement("Coinfig",
-            new XElement("Order Id", 1),
-            new XElement("Order Item Id", 200),
-            new XElement("Product Id", 124568));
-        ConfigRoot.Save(fileConfig);
-        fileConfig.Close();
+        InitProduct();
+        InitOrder();
+        InitOrderItem();
     }
-    public static void InitializationOfProducts()
+    /// <summary>
+    /// Product list initialization
+    /// </summary>
+    private static void InitProduct()
     {
-        IEnumerable<Product?> Products = from item in products                 
-                                         select InitIDPro(item ?? throw new Exception());
-        FileStream filePro = new FileStream(ProductPath, FileMode.Create);
-        XmlSerializer x = new XmlSerializer(Products.GetType());
-        x.Serialize(filePro, Products);
-        filePro.Close();
+        for (int i = 0; i < InitialNumOfproducts/*200*/; i++)
+        {//The loop will create new products and add to the list
+            Product newProduct = new Product(); //Creating an object
+            newProduct.ProductId = Config.productIndex++; //ID number by running number
+            newProduct.ProductName = "product" + i;
+            newProduct.ProductCategory = (Category)(random.Next(0, 5)); ;
+            newProduct.ProductPrice = Math.Round(random.Next(500) + random.NextDouble(), 1);
+            if (random.Next(0, 100) > 5)
+            {
+                newProduct.AmmountInStock = random.Next(20, 100);
+            }
+            else
+            {
+                newProduct.AmmountInStock = 1;
+            }
+            products.Add(newProduct);
+        }
     }
-    public static void InitializationOfOrder()
+    private static void InitOrder()
+    {//Initialization for orders
+        for (int i = 0; i < InitialNumOfOrders; i++)
+        {
+            Order newOrder = new Order();
+            newOrder.CustomerName = "Costumer " + i;
+            newOrder.CustomerAdress = "Adress " + i;
+            newOrder.CustomerEmail = "customer" + i + "@gmail.com";
+            newOrder.OrderId = Config.orderIndex++;
+            newOrder.OrderDate = DateTime.Now - new TimeSpan(random.Next(0, 5), random.Next(0, 28), random.Next(0, 12), random.Next(0, 60), random.Next(0, 60));
+            if (random.Next(0, 100) < 80)
+            {
+                newOrder.ShipDate = newOrder.OrderDate + new TimeSpan(random.Next(0, 7), random.Next(0, 12), random.Next(0, 59), random.Next(0, 59));
+                if (random.Next(0, 100) < 60)
+                    newOrder.DeliveryDate = newOrder.ShipDate + new TimeSpan(random.Next(0, 14), random.Next(0, 12), random.Next(0, 59), random.Next(0, 59));
+                else
+                    newOrder.DeliveryDate = null;
+            }
+
+            else
+            {
+                newOrder.ShipDate = null;
+                newOrder.DeliveryDate = null;
+            }
+            orders.Add(newOrder);
+        }
+    }
+    private static void InitOrderItem()
     {
-        IEnumerable<Order?> Orders = from item in orders
-                                     select InitIDOrd(item ?? throw new Exception());
-        FileStream fileOrd = new FileStream(OrderPath, FileMode.Create);
-        XmlSerializer x = new XmlSerializer(Orders.GetType());
-        x.Serialize(fileOrd, Orders);
-        fileOrd.Close();
-    }
-    public static void InitializationOfOrderItems()
-    {
-        IEnumerable<OrderItem?> OrderItems = from item in orderItems
-                                         select InitIDOrdItm(item ?? throw new Exception());
-        FileStream fileOrdIt = new FileStream(OrderItemPath, FileMode.Create);
-        XmlSerializer x = new XmlSerializer(OrderItems.GetType());
-        x.Serialize(fileOrdIt, OrderItems);
-        fileOrdIt.Close();
-    }
-    public static Product? InitIDPro(Product s)
-    { 
-        s.ProductId = Convert.ToInt32(XElement.Load(ConfigPath).Element("Product Id"));
-        return s;
-    }
-    public static Order? InitIDOrd(Order s)
-    { 
-        s.OrderId = Convert.ToInt32(XElement.Load(ConfigPath).Element("Order Id"));
-        return s;
-    }
-    public static OrderItem? InitIDOrdItm(OrderItem s)
-    { 
-        s.OrderItemId = Convert.ToInt32(XElement.Load(ConfigPath).Element("Order Item Id"));
-        return s;
+        for (int i = 0; i < 0.2 * InitialNumOfOrders; i++)
+        {
+            OrderItem newOrderItem = new OrderItem();
+            newOrderItem.OrderItemId = Config.orderItemIndex++;
+            newOrderItem.OrderId = orders[random.Next(0, InitialNumOfOrders)].Value.OrderId;
+            int index = random.Next(0, InitialNumOfproducts);
+            newOrderItem.ProductId = products[index].Value.ProductId;
+            newOrderItem.Amount = random.Next(0, 5);
+            newOrderItem.Price = products[index].Value.ProductPrice;
+            orderItems.Add(newOrderItem);
+        }
     }
 }
+//לטפל בעדכון מספר רץ
+//namespace Dal;
+
+//internal static class XMLdataSource
+//{
+//    static string ProductPath = @"xmlProduct.xml";
+//    static string OrderPath = @"xmlOrder.xml";
+//    static string OrderItemPath = @"xmlOrderItem.xml";
+//    static string ConfigPath = @"config.xml";
+
+//    static IDal dal = Factory.Get();
+//    static public List<Product?> products = dal.Product.GetAll().ToList();
+//    static public List<Order?> orders = dal.Order.GetAll().ToList();
+//    static public List<OrderItem?> orderItems = dal.OrderItem.GetAll().ToList();
+
+//    static XMLdataSource()
+//    {
+//        if(!Directory.Exists(ProductPath))
+//            Directory.CreateDirectory(ProductPath);
+//        if (!Directory.Exists(OrderPath))
+//            Directory.CreateDirectory(OrderPath);
+//        if (!Directory.Exists(OrderItemPath))
+//            Directory.CreateDirectory(OrderItemPath);
+//        if(!Directory.Exists(ConfigPath))
+//            Directory.CreateDirectory(ConfigPath);
+//    }
+//    public static void initialization()
+//    {
+//        Config();
+//        InitializationOfProducts();
+//        InitializationOfOrder();
+//        InitializationOfOrderItems();
+//    }
+//    public static void Config()
+//    {
+//        FileStream fileConfig = new FileStream(ConfigPath, FileMode.Create);
+//        XElement ConfigRoot = new XElement("Coinfig",
+//            new XElement("Order Id", 1),
+//            new XElement("Order Item Id", 200),
+//            new XElement("Product Id", 124568));
+//        ConfigRoot.Save(fileConfig);
+//        fileConfig.Close();
+//    }
+//    public static void InitializationOfProducts()
+//    {
+//        IEnumerable<Product?> Products = from item in products                 
+//                                         select InitIDPro(item ?? throw new Exception());
+//        FileStream filePro = new FileStream(ProductPath, FileMode.Create);
+//        XmlSerializer x = new XmlSerializer(Products.GetType());
+//        x.Serialize(filePro, Products);
+//        filePro.Close();
+//    }
+//    public static void InitializationOfOrder()
+//    {
+//        IEnumerable<Order?> Orders = from item in orders
+//                                     select InitIDOrd(item ?? throw new Exception());
+//        FileStream fileOrd = new FileStream(OrderPath, FileMode.Create);
+//        XmlSerializer x = new XmlSerializer(Orders.GetType());
+//        x.Serialize(fileOrd, Orders);
+//        fileOrd.Close();
+//    }
+//    public static void InitializationOfOrderItems()
+//    {
+//        IEnumerable<OrderItem?> OrderItems = from item in orderItems
+//                                         select InitIDOrdItm(item ?? throw new Exception());
+//        FileStream fileOrdIt = new FileStream(OrderItemPath, FileMode.Create);
+//        XmlSerializer x = new XmlSerializer(OrderItems.GetType());
+//        x.Serialize(fileOrdIt, OrderItems);
+//        fileOrdIt.Close();
+//    }
+//    public static Product? InitIDPro(Product s)
+//    {
+//        int ID =  Convert.ToInt32(XElement.Load(ConfigPath).Element("Product Id"));
+//        s.ProductId =ID;
+//        ID++;
+//        XElement config = XElement.Load(ConfigPath);
+//        config.Element("Order Id").SetValue(ID);
+//        config.Save(ConfigPath);
+//        return s;
+//    }
+//    public static Order? InitIDOrd(Order s)
+//    { 
+//        s.OrderId = Convert.ToInt32(XElement.Load(ConfigPath).Element("Order Id"));
+//        return s;
+//    }
+//    public static OrderItem? InitIDOrdItm(OrderItem s)
+//    { 
+//        s.OrderItemId = Convert.ToInt32(XElement.Load(ConfigPath).Element("Order Item Id"));
+//        return s;
+//    }
+//}
--- DalXml/dalOrder.cs
+++ DalXml/dalOrder.cs
@@ -6,19 +6,62 @@ using System.Xml.Linq;
 internal class dalOrder : IOrder
 {
     string path = "xmlOrder.xml";
-    XElement ordersRoot;
     public int Add(Order objToAdd)
-    { 
-    
+    {
+        List<Order> OrderLst = ToolsXML.LoadListFromXMLSerializer<Order>(path);
+
+        if (OrderLst.Exists(x => x.OrderId == objToAdd.OrderId))
+            throw new DuplicationException("Order");
+
+        OrderLst.Add(objToAdd);
+
+        ToolsXML.SaveListToXMLSerializer(OrderLst, path);
+
+        return objToAdd.OrderId;
     }
+
     public void Delete(int objToDelete)
-    { }
+    {
+        List<Order> OrderLst = ToolsXML.LoadListFromXMLSerializer<Order>(path);
+        int x= OrderLst.FindIndex(x=> x.OrderId == objToDelete);
+        if (x != -1)
+            throw new NotfoundException("Order");
+        OrderLst.RemoveAt(x);
+        ToolsXML.SaveListToXMLSerializer(OrderLst, path);
+    }
     public void Update(Order objToUpdate) 
-    { }
+    {
+        List<Order> OrderLst = ToolsXML.LoadListFromXMLSerializer<Order>(path);
+        int x = OrderLst.FindIndex(x => x.OrderId == objToUpdate.OrderId);
+        if (x != -1)
+            throw new NotfoundException("Order");
+        OrderLst[x] = objToUpdate;
+        ToolsXML.SaveListToXMLSerializer(OrderLst, path);
+        return;
+    }
     public Order GetById(int objToGet) 
-    { }
+    {
+        List<Order> OrderLst = ToolsXML.LoadListFromXMLSerializer<Order>(path);
+        int x = OrderLst.FindIndex(x => x.OrderId == objToGet);
+        if (x != -1)
+            throw new NotfoundException("Order");       
+        return OrderLst[x];
+    }
     public Order GetByCondition(Func<Order?, bool>? condition) 
-    { }
+    {
+        List<Order> OrderLst = ToolsXML.LoadListFromXMLSerializer<Order>(path);
+        int x = OrderLst.FindIndex(x => condition(x) == true);
+        if (x != -1)
+            throw new NotfoundException("Order");
+        return OrderLst[x];
+    }
     public IEnumerable<Order?> GetAll(Func<Order?, bool>? condition = null) 
-    { }
+    {
+        List<DO.Order?> OrderList = ToolsXML.LoadListFromXMLSerializer<DO.Order?>(path);
+
+        if (condition == null)
+            return OrderList.AsEnumerable().OrderByDescending(p => p?.OrderId);
+
+        return OrderList.Where(condition).OrderByDescending(p => p?.OrderId);
+    }
 }
